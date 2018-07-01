652de08df86e5bbf5bb47f430cb379a022958ae0
justin <namsangboy@buzzni.com>
2018-07-01 19:09:33 +0900

    #3 add test160, test500

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # swmaestro_image_retrieval_evaluator
 software maestro 2018 backend - image retrieval contest evaluation server
- * score leader board : http://115.68.223.177:31001/leader_board
+ * score leader board : http://eval.buzzni.net:31000/leader_board
  * data set : https://www.dropbox.com/s/pbui2jycsuimomu/fashion_retrieval_img.tar.gz?dl=0
     * query folder : query image
     * compare folder : search target image 
@@ -8,8 +8,8 @@ software maestro 2018 backend - image retrieval contest evaluation server
     1. iterate all query image and calculate distance with all compare image.
     2. sort compare image by distance (asc)
     3. upload the result (160 query)    
- * evaluation server : http://115.68.223.177:31001
- * api description : http://115.68.223.177:31001/apidocs/#/  
+ * evaluation server : http://eval.buzzni.net:31000
+ * api description : http://eval.buzzni.net:31000/apidocs/#/  
  * sample upload evaluation code
   ```python
 system_result_dict = {}
@@ -18,14 +18,15 @@ for each in glob("/home/maestro_2018/test/img/query/*"):
     score_dict = {}
     for other in glob("/home/maestro_2018/test/img/compare/*"):
         fname2 = other.split("/")[-1]
-        dist = cosine(res18_img_feature_dict[fname], res18_img_feature_dict[fname2])
+        dist = cosine(img_feature_dict[fname], img_feature_dict[fname2])
         score_dict[fname2] = dist
     sorted_list = sorted(score_dict.items(), key=lambda (k,v):(v,k), reverse=False)
     qid = fname.split("_")[-1].split(".")[0]
-    system_result_dict[qid] = map(lambda i : i[0], sorted_list[:10])
+    system_result_dict[qid] = map(lambda i : i[0], sorted_list[:20])
           
 name = 'buzzni-200'
 nickname = 'buzzni-200'
-r = requests.post('http://115.68.223.177:31001', json={"pred_result": system_result_dict,'name':name, 'nickname':nickname})
+r = requests.post('http://eval.buzzni.net:31000', json={"pred_result": system_result_dict,'name':name, 'nickname':nickname})
 print r.json()
 ```  
+ * 버즈니에서 개발자 채용을 하고 있습니다 관심 있는 분들은 recruit@buzzni.com 로 연락 주세요. 전문연구요원, 보충역 가능합니다.
--- evaluation_server.py
+++ evaluation_server.py
@@ -1,63 +1,57 @@
 # coding:utf-8
 import sys
-import numpy as np
-from time import sleep
-from random import sample
-from glob import glob
 
-from sklearn.externals import joblib
-from flask import Flask, request, jsonify, render_template, request
-from flask_restful import Api
-from flask_restful import Resource
-from flasgger import Swagger
-import pyltr
-from pymongo import MongoClient, DESCENDING
+import numpy as np
 import pandas as pd
 import pendulum
+import pyltr
+from flasgger import Swagger
+from flask import Flask, jsonify, render_template, request
+from pymongo import MongoClient, DESCENDING
+from sklearn.externals import joblib
 
 import config
 
 app = Flask(__name__)
 swagger = Swagger(app)
 
-metric = pyltr.metrics.NDCG(k=10)
 client = MongoClient('mongodb://%s:%s@%s' % (config.mongo_username,
                                              config.mongo_password,
                                              config.mongo_host))
 
-score_history_db = client['soma_2018']['score_history']
-score_db = client['soma_2018']['score']
+SCORE_DB = client['soma_2018']
+score_history_db = SCORE_DB['score_history']
 
-qid_gold_list_dict = {}
-with open(config.gold_file_path) as fin:
-    data = fin.read().strip()
-    for line in data.split("\n"):
-        qid, data = line.strip().split(" | ")
-        qid_gold_list_dict[qid] = data.split(",")
-
-candidate_list = []
-for each in glob(config.candidate_file_path+"/*"):
-    candidate_list.append(each.split("/")[-1])
+eval_qid_gold_list_dict = joblib.load(config.eval_gold_file_path)
+test160_qid_gold_list_dict = joblib.load(config.test160_gold_file_path)
+test500_qid_gold_list_dict = joblib.load(config.test500_gold_file_path)
+qid_gold_list_dict = {'eval': eval_qid_gold_list_dict,
+                      'test160': test160_qid_gold_list_dict,
+                      'test500': test500_qid_gold_list_dict}
 
 
-def add_score_data(name, nickname, score):
+def add_score_data(name, nickname, score, email, mode, ip, day):
     '''
     점수 기록을 score_history 와 score 에 추가한다
     단 score 에 추가할때에는 점수가 더 좋을때만 추가한다.
     @param name: 실명
     @param nickname: 점수판 표시용 이름
     @param score: 검색 성능 점수
+    @param email: email
     @return:
     '''
-    score_history_db.insert_many([{'nickname': nickname, 'name': name, 'score': score, 'date': pendulum.now()}])
-    prev_score_info = score_db.find_one({'_id': name})
+    score_history_db.insert_many([{'nickname': nickname, 'name': name, 'score': score, 'date': pendulum.now(),
+                                   'email': email, 'mode': mode, 'ip': ip, 'day': day}])
+    prev_score_info = SCORE_DB[mode + "_score"].find_one({'nickname': nickname})
     if prev_score_info:
         if prev_score_info['score'] > score:
             '''
             새로 업데이트 하는 것의 점수가 이전 최고 기록보다 점수가 낮은 경우이다. 이때는 점수를 업데이트 하지 않는다.
             '''
             return False
-    score_db.update_one({'_id': name}, {'$set': {'nickname': nickname, 'score': score, 'date': pendulum.now()}}, upsert=True)
+    SCORE_DB[mode + "_score"].update_one({'nickname': nickname},
+                                         {'$set': {'name': name, 'nickname': nickname, 'score': score,
+                                                   'date': pendulum.now(), 'email': email}}, upsert=True)
     return True
 
 
@@ -66,10 +60,19 @@ def evaluation():
     '''
     pred_result 에 대한 ndcg 성능 평가 결과를 반환한다.
     평가 서버에 요청할때에는 아래와 같이 요청 가능하다.
-    r = requests.post('http://115.68.223.177:31001', json={"pred_result": system_result_dict})
+    mode 는  eval, test160, test500 3가지가 가능하다. eval 는 파라미터 최적화 할때 사용하고 여기서 최적화된 파라미터로 test160/test500 에 최종 성능을 평가한다.
+    test160 는 한 ip 당 일 1회 요청 제한이 있다.
+    r = requests.post('http://eval.buzzni.net:31000', json={"pred_result": system_result_dict})
     print r.json()
 
-    system_result_dict 에는 query_id 별로 해당 query image 와 가장 비슷한 10개의 이미지 id 리스트를 가지고 있다.
+    name = 'gil-dong Hong'
+    nickname = 'gil-dong'
+    email = 'email'
+    mode = 'test160'
+    r = requests.post('http://eval.buzzni.net:31000', json={"pred_result": system_result_dict,'name':name, 'nickname':nickname, 'mode':mode,'email':email})
+    print (r.json())
+
+    system_result_dict 에는 query_id 별로 해당 query image 와 가장 비슷한 20개의 이미지 id 리스트를 가지고 있다.
     이미지 id 의 순서는 더 비슷한 순서대로 들어 있어야 한다.
     {'1': ['57aaa9e3efd3e84d8d906cd95c6da6a9.jpg',
       'd6c86e7ae73003d8c848baa0a95720f9.jpg' .. ]
@@ -91,6 +94,16 @@ def evaluation():
         in: query
         type: string
         required: true
+
+      - name: email
+        in: query
+        type: string
+        required: true
+
+      - name: mode
+        in: query
+        type: string
+        required: true
     responses:
       200:
         description: prediction score
@@ -100,19 +113,31 @@ def evaluation():
     system_result_dict = request.get_json()['pred_result']
     name = request.get_json().get('name', '')
     nickname = request.get_json().get('nickname', '')
-
+    email = request.get_json().get('email', '')
+    mode = request.get_json().get('mode', 'eval')
+    ip = request.remote_addr
+    now_day = pendulum.now().format('%Y%m%d')
     if not name:
-        return jsonify({'msg': 'name parameter required! - http://115.68.223.177:31001/apidocs/#/default/post_'})
+        return jsonify({'msg': 'name parameter required! - http://eval.buzzni.net:31000/apidocs/#/default/post_'})
     if not nickname:
-        return jsonify({'msg': 'nickname parameter required! - http://115.68.223.177:31001/apidocs/#/default/post_'})
+        return jsonify({'msg': 'nickname parameter required! - http://eval.buzzni.net:31000/apidocs/#/default/post_'})
+    if mode not in ['eval', 'test160', 'test500']:
+        return jsonify({'msg': 'mode value (eval or test) parameter required! - http://eval.buzzni.net:31000/apidocs/#/default/post_'})
+    if not email:
+        return jsonify({'msg': 'email parameter required! - http://eval.buzzni.net:31000/apidocs/#/default/post_'})
+    mode_size_dict = {'eval': 160, 'test160': 160, 'test500': 495}
 
-    if len(system_result_dict.keys()) != 160:
+    if len(system_result_dict.keys()) != mode_size_dict[mode]:
         return jsonify({'msg': 'expected result num is 160, but current result num is %d' % len(system_result_dict.keys())})
+    if mode in ['test160']:
+        prev_history = score_history_db.find_one({'mode': mode, 'ip': ip, 'day': now_day})
+        if prev_history:
+            return jsonify({'msg': 'you can submit only one result in a day'})
 
     search_gold_y_list = []
     search_system_y_list = []
     search_qid_list = []
-    for qid in qid_gold_list_dict.keys():
+    for qid in qid_gold_list_dict[mode].keys():
         system_key_score_dict = {}
 
         for idx, k in enumerate(system_result_dict[qid]):
@@ -121,47 +146,65 @@ def evaluation():
             '''
             if idx < 3:
                 system_key_score_dict[k] = 2
-            elif idx < len(qid_gold_list_dict[qid]) / 2.0:
+            elif idx < len(qid_gold_list_dict[mode][qid]) / 2.0:
                 '''
                 query 에 따라서 gold 에 포함되는 이미지 개수가 다 가변적이라서, 위와 같이 하였다.
                 '''
                 system_key_score_dict[k] = 1.5
 
-            elif idx < len(qid_gold_list_dict[qid]):
+            elif idx < len(qid_gold_list_dict[mode][qid]):
                 system_key_score_dict[k] = 1
 
         key_gold_score_dict = {}
-        for key in qid_gold_list_dict[qid]:
+        for key in qid_gold_list_dict[mode][qid]:
             key_gold_score_dict[key] = 1
-
-        for key in list(system_key_score_dict.keys())[:len(qid_gold_list_dict[qid])]:
+        max_limit = np.min([len(qid_gold_list_dict[mode][qid]), 10])
+        # for key in list(system_key_score_dict.keys())[:len(qid_gold_list_dict[mode][qid])]:
+        for key in list(system_key_score_dict.keys())[:max_limit]:
             search_qid_list.append(qid)
             search_gold_y_list.append(key_gold_score_dict.get(key, 0))
             search_system_y_list.append(system_key_score_dict.get(key))
-
+    # 평가 할때마다 만들어줘야함
+    metric = pyltr.metrics.NDCG(k=10)
     score = metric.calc_mean(search_qid_list, np.asarray(search_gold_y_list), np.asarray(search_system_y_list))
     if score > 1:
         return jsonify({'msg': 'invalid score ' + str(score)})
     print('score:', score)
     result = {'score': score}
-    add_score_data(name=name, nickname=nickname, score=score)
+    add_score_data(name=name, nickname=nickname, score=score, email=email, mode=mode, ip=ip, day=now_day)
     return jsonify(result)
 
 
 @app.route("/leader_board")
 def show_leader_board():
+    filter_mode = request.args.get("mode", '')
+    score_data_list = []
+    titles = ['na']
+    for mode in ['test160', 'eval', 'test500']:
+        if filter_mode:
+            if mode != filter_mode:
+                continue
 
-    score_list = []
-    rank = 1
-    for each in score_db.find().sort([("score", DESCENDING)]):
-        score_list.append({'rank': rank, 'name': each['nickname'], 'score': each['score']})
-        rank += 1
-    data = pd.DataFrame(score_list)
-    data.set_index(['name'], inplace=True)
-    data.index.name = None
-    return render_template('leader_board.html', tables=[data.to_html()], titles=['na', 'Leader Board'])
+        score_list = []
+        rank = 1
+        for each in SCORE_DB[mode + "_score"].find().sort([("score", DESCENDING)]).limit(300):
+            score_list.append({'rank': rank, 'name': each['nickname'], 'score': each['score']})
+            rank += 1
+        if not score_list:
+            continue
+
+        data = pd.DataFrame(score_list)
+        data.set_index(['name'], inplace=True)
+        data.index.name = None
+        score_data_list.append(data.to_html())
+        titles.append(mode + " Leaderbaord")
+
+    return render_template('leader_board.html', tables=score_data_list, titles=titles)
+
 
 if __name__ == '__main__':
     # test()
     port = int(sys.argv[1])
+    print("port")
+    app.debug = True
     app.run(host='0.0.0.0', port=int(sys.argv[1]))
